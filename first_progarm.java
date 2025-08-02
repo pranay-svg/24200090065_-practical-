138431395b53f15daec2bb7358700b27dd657225
pranay-svg <padakantipranaykumar@gmail.com>
2025-08-02 09:35:36 +0530

    Add files via upload

---
+++ first_progarm.java
@@ -0,0 +1,19 @@
+package my_project;
+
+public class first_progarm {
+	public static void main(String args[]) {
+		int a = 2;
+		char b = 'A';
+		double c = 3.6;
+		float d = 5.6f;
+		String s = "hello this is cse";
+		//System.out.println(a);
+		//System.out.println(b);
+		//System.out.println(c);
+		//System.out.println(d);
+		//System.out.println(s);
+		//System.out.println(s + " "+a + " "+b + " "+c + " "+d);
+		System.out.printf("integer %d%ncharacter %c%ndouble %f%nstring %s",a,b,c,d,s);
+	}
+
+}
